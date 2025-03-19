8f69b7030ff7a19da6a6d0eb10657856f2d107bd
sebgonzalezo <s.gonzalez.o@ug.uchile.cl>
2025-03-19 18:48:00 -0300

    change: usuario.py

--- usuario.py
+++ usuario.py
@@ -11,7 +11,5 @@ class Usuario:
     def listarTareas(self):
         for tarea in self.tareas:
             if tarea.estaLista():
-                print(f"[X] {tarea.obtenerNombre()}" )
-            else:
-                print(f"[ ] {tarea.obtenerNombre()}" )
-    
+                print(f"La tarea {tarea.obtenerNombre()} está lista")
+            
